6669b5d830609889916be3b0d3d059e675f7a113
rolancia <95jjangjun@gmail.com>
2022-05-04 00:03:52 +0900

    리팩토링 chap1 예시 작성(js, ts)

---
+++ refactoring/chap1/main.js
@@ -0,0 +1,49 @@
+var invoices = require('./invoices.json');
+var plays = require('./plays.json');
+function statement(invoice, plays) {
+    var totalAmount = 0;
+    var volumeCredits = 0;
+    var result = "\uCCAD\uAD6C \uB0B4\uC5ED (\uACE0\uAC1D\uBA85 : " + invoice.customer + ")\n";
+    var format = new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD',
+        minimumFractionDigits: 2
+    }).format;
+    for (var _i = 0, _a = invoice.performances; _i < _a.length; _i++) {
+        var perf = _a[_i];
+        var play = plays[perf.playID];
+        var thisAmount = 0;
+        switch (play.type) {
+            case 'tragedy':
+                thisAmount = 40000;
+                if (perf.audience > 30) {
+                    thisAmount += 1000 * (perf.audience - 30);
+                }
+                break;
+            case 'comedy':
+                thisAmount = 30000;
+                if (perf.audience > 20) {
+                    thisAmount += 10000 + 500 * (perf.audience - 20);
+                }
+                thisAmount += 300 * perf.audience;
+                break;
+            default:
+                throw new Error("unknown type " + play.type);
+        }
+        // 포인트 적립
+        volumeCredits += Math.max(perf.audience - 30, 0);
+        // 희극 관객 5명마다 추가 포인트 제공
+        if ('comedy' === play.type) {
+            volumeCredits += Math.floor(perf.audience / 5);
+        }
+        // 청구 내역 출력
+        result += play.name + ": " + format(thisAmount / 100) + " (" + perf.audience + "\uC11D)\n";
+        totalAmount += thisAmount;
+    }
+    result += "\uCD1D\uC561: " + format(totalAmount / 100) + "\n";
+    result += "\uC801\uB9BD \uD3EC\uC778\uD2B8: " + volumeCredits + "\uC810\n";
+    return result;
+}
+invoices.forEach(function (invoice) {
+    console.log(statement(invoice, plays));
+});
