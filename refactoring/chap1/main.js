3aa3a5efd358b7481abd056c0c5bfe3b4af78042
rolancia <95jjangjun@gmail.com>
2022-05-23 23:07:11 +0900

    revert main.js & change main.ts name

--- refactoring/chap1/main.js
+++ refactoring/chap1/main.js
@@ -1,49 +1,55 @@
-var invoices = require('./invoices.json');
-var plays = require('./plays.json');
+const invoices = require('./invoices.json');
+const plays = require('./plays.json');
+
 function statement(invoice, plays) {
-    var totalAmount = 0;
-    var volumeCredits = 0;
-    var result = "\uCCAD\uAD6C \uB0B4\uC5ED (\uACE0\uAC1D\uBA85 : " + invoice.customer + ")\n";
-    var format = new Intl.NumberFormat('en-US', {
-        style: 'currency',
-        currency: 'USD',
-        minimumFractionDigits: 2
-    }).format;
-    for (var _i = 0, _a = invoice.performances; _i < _a.length; _i++) {
-        var perf = _a[_i];
-        var play = plays[perf.playID];
-        var thisAmount = 0;
-        switch (play.type) {
-            case 'tragedy':
-                thisAmount = 40000;
-                if (perf.audience > 30) {
-                    thisAmount += 1000 * (perf.audience - 30);
-                }
-                break;
-            case 'comedy':
-                thisAmount = 30000;
-                if (perf.audience > 20) {
-                    thisAmount += 10000 + 500 * (perf.audience - 20);
-                }
-                thisAmount += 300 * perf.audience;
-                break;
-            default:
-                throw new Error("unknown type " + play.type);
+  let totalAmount = 0;
+  let volumeCredits = 0;
+  let result = `청구 내역 (고객명 : ${invoice.customer})\n`;
+
+  const format = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+    minimumFractionDigits: 2,
+  }).format;
+
+  for (let perf of invoice.performances) {
+    const play = plays[perf.playID];
+    let thisAmount = 0;
+
+    switch (play.type) {
+      case 'tragedy':
+        thisAmount = 40000;
+        if (perf.audience > 30) {
+          thisAmount += 1000 * (perf.audience - 30);
         }
-        // 포인트 적립
-        volumeCredits += Math.max(perf.audience - 30, 0);
-        // 희극 관객 5명마다 추가 포인트 제공
-        if ('comedy' === play.type) {
-            volumeCredits += Math.floor(perf.audience / 5);
+        break;
+      case 'comedy':
+        thisAmount = 30000;
+        if (perf.audience > 20) {
+          thisAmount += 10000 + 500 * (perf.audience - 20);
         }
-        // 청구 내역 출력
-        result += play.name + ": " + format(thisAmount / 100) + " (" + perf.audience + "\uC11D)\n";
-        totalAmount += thisAmount;
+        thisAmount += 300 * perf.audience;
+        break;
+      default:
+        throw new Error(`unknown type ${play.type}`);
     }
-    result += "\uCD1D\uC561: " + format(totalAmount / 100) + "\n";
-    result += "\uC801\uB9BD \uD3EC\uC778\uD2B8: " + volumeCredits + "\uC810\n";
-    return result;
+    // 포인트 적립
+    volumeCredits += Math.max(perf.audience - 30, 0);
+    // 희극 관객 5명마다 추가 포인트 제공
+    if ('comedy' === play.type) {
+      volumeCredits += Math.floor(perf.audience / 5);
+    }
+
+    // 청구 내역 출력
+    result += `${play.name}: ${format(thisAmount / 100)} (${perf.audience}석)\n`;
+    totalAmount += thisAmount;
+  }
+
+  result += `총액: ${format(totalAmount / 100)}\n`;
+  result += `적립 포인트: ${volumeCredits}점\n`;
+  return result;
 }
-invoices.forEach(function (invoice) {
-    console.log(statement(invoice, plays));
+
+invoices.forEach((invoice) => {
+  console.log(statement(invoice, plays));
 });
