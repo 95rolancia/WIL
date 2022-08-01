801c1e564364a243904bc9c9489eac253e19c108
jjangjun <95jjangjun@gmail.com>
2022-08-01 21:44:59 +0900

    [1.4 statement() 쪼개기] format 함수 선언 바꾸기

--- refactoring/chap1/main.js
+++ refactoring/chap1/main.js
@@ -6,22 +6,22 @@ export function statement(invoice, plays) {
   for (let perf of invoice.performances) {
     volumeCredits += volumeCreditsFor(perf);
     // 청구 내역 출력
-    result += `${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${
+    result += `${playFor(perf).name}: ${usd(amountFor(perf))} (${
       perf.audience
     }석)\n`;
     totalAmount += amountFor(perf);
   }
 
-  result += `총액: ${format(totalAmount / 100)}\n`;
+  result += `총액: ${usd(totalAmount)}\n`;
   result += `적립 포인트: ${volumeCredits}점\n`;
   return result;
 
-  function format(aNumber) {
+  function usd(aNumber) {
     return new Intl.NumberFormat("en-US", {
       style: "currency",
       currency: "USD",
       minimumFractionDigits: 2,
-    }).format(aNumber);
+    }).format(aNumber / 100);
   }
 
   function volumeCreditsFor(perf) {
