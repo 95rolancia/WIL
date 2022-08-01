4e1a8a4863c408104dc659949016001660d9c2c7
jjangjun <95jjangjun@gmail.com>
2022-08-01 22:00:22 +0900

    [1.4 statement() 쪼개기] totalAmount 변수 인라인하고 이름 이쁘게

--- refactoring/chap1/main.js
+++ refactoring/chap1/main.js
@@ -7,13 +7,11 @@ export function statement(invoice, plays) {
     }석)\n`;
   }
 
-  let totalAmount = appleSauce();
-
-  result += `총액: ${usd(totalAmount)}\n`;
+  result += `총액: ${usd(totalAmount())}\n`;
   result += `적립 포인트: ${totalVolumeCredits()}점\n`;
   return result;
 
-  function appleSauce() {
+  function totalAmount() {
     let totalAmount = 0;
     for (let perf of invoice.performances) {
       totalAmount += amountFor(perf);
