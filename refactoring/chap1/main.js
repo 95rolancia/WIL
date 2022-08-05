6fc69de77a680b261a82f8c208d81b7f333da6e6
jjangjun <95jjangjun@gmail.com>
2022-08-05 22:10:00 +0900

    단계쪼개기 > 데이터 생성, 데이터 처리 완전 분리!

--- refactoring/chap1/main.js
+++ refactoring/chap1/main.js
@@ -1,10 +1,14 @@
 export function statement(invoice, plays) {
-  const statementData = {};
-  statementData.customer = invoice.customer;
-  statementData.performances = invoice.performances.map(enrichPerformance);
-  statementData.totalAmount = totalAmount(statementData);
-  statementData.totalVolumeCredits = totalVolumeCredits(statementData);
-  return renderPlainText(statementData, plays);
+  return renderPlainText(createStatementData(invoice, plays));
+
+  function createStatementData(invoice, plays) {
+    const statementData = {};
+    statementData.customer = invoice.customer;
+    statementData.performances = invoice.performances.map(enrichPerformance);
+    statementData.totalAmount = totalAmount(statementData);
+    statementData.totalVolumeCredits = totalVolumeCredits(statementData);
+    return statementData;
+  }
 
   function enrichPerformance(aPerformance) {
     const result = Object.assign({}, aPerformance);
@@ -52,11 +56,14 @@ export function statement(invoice, plays) {
   }
 
   function totalAmount(data) {
-    return data.performances.reduce((total, p) => total += p.amount, 0);
+    return data.performances.reduce((total, p) => (total += p.amount), 0);
   }
 
   function totalVolumeCredits(data) {
-    return data.performances.reduce((total, p) => total += p.volumeCredits, 0)
+    return data.performances.reduce(
+      (total, p) => (total += p.volumeCredits),
+      0
+    );
   }
 }
 
