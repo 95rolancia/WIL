f7c8bbb110c9b1456c14886d90720d975de1bf3b
jjangjun <95jjangjun@gmail.com>
2022-08-05 22:29:24 +0900

    단계쪼개기 > 파일 분리 & 테스트 코드 재작성

--- refactoring/chap1/data.js
+++ refactoring/chap1/data.js
@@ -1,25 +1,23 @@
-export const invoices = [
-  {
-    customer: 'BigCo',
-    performances: [
-      {
-        playID: 'hamlet',
-        audience: 55,
-      },
-      {
-        playID: 'as-like',
-        audience: 35,
-      },
-      {
-        playID: 'othello',
-        audience: 40,
-      },
-    ],
-  },
-];
+export const invoices = {
+  customer: "BigCo",
+  performances: [
+    {
+      playID: "hamlet",
+      audience: 55,
+    },
+    {
+      playID: "as-like",
+      audience: 35,
+    },
+    {
+      playID: "othello",
+      audience: 40,
+    },
+  ],
+};
 
 export const plays = {
-  hamlet: { name: 'Hamlet', type: 'tragedy' },
-  'as-like': { name: 'As You Like It', type: 'comedy' },
-  othello: { name: 'Othello', type: 'tragedy' },
+  hamlet: { name: "Hamlet", type: "tragedy" },
+  "as-like": { name: "As You Like It", type: "comedy" },
+  othello: { name: "Othello", type: "tragedy" },
 };
--- refactoring/chap1/main.test.js
+++ refactoring/chap1/main.test.js
@@ -1,12 +1,11 @@
-import { statement } from './main.js';
+import createStatementData from './createStatementData.js';
 import { invoices, plays } from './data.js';
+import { renderPlainText } from './statement.js';
 
 describe('test statement', () => {
   it('statement', () => {
-    let result = '';
-    invoices.forEach((invoice) => {
-      result += statement(invoice, plays);
-    });
+    const data = createStatementData(invoices, plays);
+    const result = renderPlainText(data);
 
     expect(result).toBe(
       `청구 내역 (고객명 : BigCo)\nHamlet: $650.00 (55석)\nAs You Like It: $580.00 (35석)\nOthello: $500.00 (40석)\n총액: $1,730.00\n적립 포인트: 47점\n`
