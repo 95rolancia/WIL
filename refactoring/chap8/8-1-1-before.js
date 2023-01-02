dd8ce4e366616816cca5d5b56050a0768ed86c7f
jjangjun <95jjangjun@gmail.com>
2023-01-02 22:09:47 +0900

    8.1 함수 옮기기

---
+++ refactoring/chap8/8-1-1-before.js
@@ -0,0 +1,55 @@
+// 8.1.1 함수 옮기기 책(8.1) Before
+
+export function trackSummary(points) {
+  const totalTime = calculateTime();
+  const totalDistance = calculateDistance();
+  const pace = totalTime / 60 / totalDistance;
+  return {
+    time: totalTime,
+    distance: totalDistance,
+    pace: pace,
+  };
+
+  function calculateDistance() {
+    let result = 0;
+    for (let i = 1; i < points.length; i++) {
+      result += distance(points[i - 1], points[i]);
+    }
+    return result;
+  }
+
+  function distance(p1, p2) {
+    // 포뮬라: http://www.movable-type.co.uk/scripts/latlong.html
+    const EARTH_RADIUS = 3959; // in miles
+    const dLat = radians(p2.lat) - radians(p1.lat);
+    const dLon = radians(p2.lon) - radians(p1.lon);
+    const a =
+      Math.pow(Math.sin(dLat / 2), 2) +
+      Math.cos(radians(p2.lat)) *
+        Math.cos(radians(p1.lat)) *
+        Math.pow(Math.sin(dLon / 2), 2);
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    return EARTH_RADIUS * c;
+  }
+  // 그 어떤 데이터와도 연관되어있지 않은 독립적인 함수이므로 밖으로 뺀다.
+  function radians(degrees) {
+    return (degrees * Math.PI) / 180;
+  }
+
+  function calculateTime() {
+    return 10000;
+  }
+}
+
+const newYork = {
+  lat: 40.73061,
+  lon: -73.935242,
+};
+
+const tokyo = {
+  lat: 35.652832,
+  lon: 139.839478,
+};
+
+const summary = trackSummary([newYork, tokyo]);
+console.log(summary);
