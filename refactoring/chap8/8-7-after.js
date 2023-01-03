e1107d255ea9b0991aa72bac9bbbd8d239d97514
jjangjun <95jjangjun@gmail.com>
2023-01-03 22:38:27 +0900

    8.7 반복문 쪼개기

---
+++ refactoring/chap8/8-7-after.js
@@ -0,0 +1,13 @@
+// 8.7 반복문 쪼개기
+
+export function reportYoungestAgeAndTotalSalary(people) {
+  return `youngestAge: ${youngestAge()}, totalSalary: ${totalSalary()}`;
+
+  function youngestAge() {
+    return Math.min(...people.map((p) => p.age));
+  }
+
+  function totalSalary() {
+    return people.reduce((total, p) => (total += p.salary), 0);
+  }
+}
