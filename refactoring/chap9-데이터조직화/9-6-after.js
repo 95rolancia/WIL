336b80cfe6a7de5b64bcdfd7fa56d68bd68d0b74
jjangjun <95jjangjun@gmail.com>
2023-01-05 21:40:12 +0900

    9.6 매직 리터럴 바꾸기

---
+++ refactoring/chap9-데이터조직화/9-6-after.js
@@ -0,0 +1,7 @@
+// 9.6 매직 리터럴 바꾸기 After
+
+const STANDARD_GRAVITY = 9.81
+
+function potentialEnergy(mass, height) {
+  return mass * STANDARD_GRAVITY * height;
+}
---
+++ refactoring/chap9-데이터조직화/9-6-before.js
@@ -0,0 +1,5 @@
+// 9.6 매직 리터럴 바꾸기 Before
+
+function potentialEnergy(mass, height) {
+  return mass * 9.81 * height;
+}
