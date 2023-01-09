f4df815015f4c2e342d9c771d36561985f2ebac8
jjangjun <95jjangjun@gmail.com>
2023-01-09 21:24:00 +0900

    10.6 어서션 추가하기

---
+++ refactoring/chap10-조건부로직간소화/10-6-after.js
@@ -0,0 +1,15 @@
+// 10.6 어서션 추가하기 After
+
+import { strict as assert } from "node:assert";
+
+class Customer {
+  constructor() {
+    this.discountRate = 0;
+  }
+  applyDiscount(number) {
+    assert(number >= 0);
+    return this.discountRate ? number - this.discountRate * number : number;
+  }
+}
+
+new Customer().applyDiscount(-5);
