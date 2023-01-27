1109e921ad1869e87a0528df8e273690d3f32562
jjangjun <95jjangjun@gmail.com>
2023-01-27 21:57:55 +0900

    11.6 질의 함수를 매개변수로 바꾸기

--- refactoring/chap11-api/11-6.js
+++ refactoring/chap11-api/11-6.js
@@ -1,7 +1,13 @@
-targetTemperature(aPlan);
+// 11.6 질의 함수를 매개변수로 바꾸기
+
+// 문제: 심하게 커플링이 되어있다.
+
+let currentTemperature = -21;
+
+targetTemperature(aPlan, thermostat.currentTemperature);
 
 // 다른모듈에 있는 함수라고 가정
-function targetTemperature(aPlan) {
-  currentTemperature = thermostat.currentTemperature;
+function targetTemperature(plan, currentTemp) {
+  currentTemperature = currentTemp;
   // ...
 }
